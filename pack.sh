0c1f773da652d46a1a11f64a2a4001fab64cf2f2
Serge ALEXANDRE <sa@broadsoftware.com>
2025-03-25 19:52:14 +0100

    Initial commit

---
+++ pack.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#export OCI_REPO_PREFIX=quay.io/kubocd/applications
+export OCI_REPO_PREFIX=localhost:5001/applications
+#export OCI_REPO_PREFIX=ghcr.io/kubocd/applications
+
+export MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+for app in apps/*
+do
+  echo
+  echo "=============================================== Will pack $app"
+  kubocd pack $app
+done
