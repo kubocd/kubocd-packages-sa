8e7eb5f6684fe2b4fe31b93d280f182a3bda46b2
Serge ALEXANDRE <sa@broadsoftware.com>
2025-03-26 17:56:34 +0100

    feat: Added cert-manager

---
+++ kcd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export KUBOCD=$( cd "$MYDIR/../kubocd/" && pwd)
+
+cd "${KUBOCD}" &&  if ! make build-kubocd; then exit $?; fi >/dev/stderr
+
+cd "${MYDIR}" || exit
+
+#export OCI_REPO_PREFIX=quay.io/kubocd/applications
+export OCI_REPO_PREFIX=localhost:5001/applications
+#export OCI_REPO_PREFIX=ghcr.io/kubocd/applications
+
+set +x
+"${KUBOCD}/bin/kubocd" "$@"
+
