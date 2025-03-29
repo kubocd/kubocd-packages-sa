ec41bcc5e7f4f6e81dd5a7a12e9ec9101c2a0446
Serge ALEXANDRE <sa@broadsoftware.com>
2025-03-29 15:09:30 +0100

    feat: bootstrap now OK

--- kcd.sh
+++ kcd.sh
@@ -9,8 +9,8 @@ cd "${KUBOCD}" &&  if ! make build-kubocd; then exit $?; fi >/dev/stderr
 cd "${MYDIR}" || exit
 
 #export OCI_REPO_PREFIX=quay.io/kubocd/applications
-export OCI_REPO_PREFIX=localhost:5001/applications
-#export OCI_REPO_PREFIX=ghcr.io/kubocd/applications
+#export OCI_REPO_PREFIX=localhost:5001/applications
+export OCI_REPO_PREFIX=ghcr.io/sergealexandre/kubocd/applications
 
 set +x
 "${KUBOCD}/bin/kubocd" "$@"
