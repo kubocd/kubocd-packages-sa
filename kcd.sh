5c2f654c60acc64fc0326d57d3ade75f22c2da0c
Serge ALEXANDRE <sa@broadsoftware.com>
2025-04-02 23:51:34 +0200

    fix: The bundle stuff was unable to generate secrets

--- kcd.sh
+++ kcd.sh
@@ -8,8 +8,8 @@ cd "${KUBOCD}" &&  if ! make build-kubocd; then exit $?; fi >/dev/stderr
 
 cd "${MYDIR}" || exit
 
-export OCI_REPO_PREFIX=quay.io/kubocd/applications
-#export OCI_REPO_PREFIX=localhost:5001/applications
+#export OCI_REPO_PREFIX=quay.io/kubocd/applications
+export OCI_REPO_PREFIX=localhost:5001/applications
 #export OCI_REPO_PREFIX=ghcr.io/sergealexandre/kubocd/applications
 
 set +x
