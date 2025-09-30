54ca831e2205e372dfbefbe0764f8c0df22a5d1e
Serge ALEXANDRE <sa@broadsoftware.com>
2025-09-30 12:33:23 +0200

    kubauth-grant and kubauth-server package

--- kcd.sh
+++ kcd.sh
@@ -8,7 +8,8 @@ cd "${KUBOCD}" &&  if ! make build-kubocd; then exit $?; fi >/dev/stderr
 
 cd "${MYDIR}" || exit
 
-export OCI_REPO_PREFIX=quay.io/kubocd/packages
+export OCI_REPO_PREFIX=quay.io/kubosa/packages
+#export OCI_REPO_PREFIX=quay.io/kubocd/packages
 #export OCI_REPO_PREFIX=localhost:5001/packages
 #export OCI_REPO_PREFIX=ghcr.io/sergealexandre/kubocd/packages
 
